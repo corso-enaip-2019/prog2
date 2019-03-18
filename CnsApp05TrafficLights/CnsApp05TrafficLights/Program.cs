e7bcb12de47675ad71363d3e7f0fc01dce15e69e
triprog-2 <triprog-2@lab.local>
2019-03-18 14:03:09 +0100

    02bennic - Aggiunta CnsApp05TrafficLights - Ancora incompleto

---
+++ CnsApp05TrafficLights/CnsApp05TrafficLights/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnsApp05TrafficLights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("North  |   |###");
+            Console.WriteLine("###|   |   |###");
+            Console.WriteLine("###|NNN|   |###");
+            Console.WriteLine("---+   |   +---");
+            Console.WriteLine("       |      W");
+            Console.WriteLine("       |      e");
+            Console.WriteLine("       |      s");
+            Console.WriteLine("-------+------t");
+            Console.WriteLine("E      |       ");
+            Console.WriteLine("a      |       ");
+            Console.WriteLine("s      |       ");
+            Console.WriteLine("t--+   |   +---");
+            Console.WriteLine("###|   |SSS|###");
+            Console.WriteLine("###|...|   |###");
+            Console.WriteLine("###|..South|###");
+
+            Console.ReadLine();
+        }
+    }
+}
