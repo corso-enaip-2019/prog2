a0caf42fb2ec74a03bc92c64acd4a243fcc79612
Nic Ben <AccntTmprnENAIP02@outlook.it>
2019-07-19 17:10:19 +0200

    bennic - Creazione progetto Libreria.

---
+++ CnsApp31P19Libreria/CnsApp31P19Libreria/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CnsApp31P19Libreria.Models
+{
+    public class Author
+    {
+        public int IDAuthor { get; set; }
+        public string AuthorCompleteName { get; set; }
+    }
+}
---
+++ CnsApp31P19Libreria/CnsApp31P19Libreria/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CnsApp31P19Libreria.Models
+{
+    public class Book
+    {
+        public int IDBook { get; set; }
+        public string Title { get; set; }
+    }
+}
